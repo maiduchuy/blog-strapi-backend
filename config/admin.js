235bac8cde03454ae68b71d7635699ea8652301a
Mai Duc Huy <huymd@dgroup.co>
2022-03-16 21:43:03 +0700

    added api token salt config

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'tucD4g79XK9uTiaFoo9YWekDTXLYes6H'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '5f5c2a79abd7d447bc471c8f29061052'),
   },
